7ee16b3ba0cc4b54efd406a7a35ac972fc6b65c7
Yz_4230 <yuzupon1480@gmail.com>
2020-10-05 18:32:05 +0900

    Better performance.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,13 @@ extern crate serde_json;
 use regex::Regex;
 use reqwest::blocking::Client;
 use reqwest::header;
-use std::fs::OpenOptions;
 use std::io::Write;
+use std::{fs, io};
 
 const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36,gzip(gfe) ";
 const POST_BODY: &str = r#"{"hidden": false, "context": {"client": {"hl": "en", "gl": "JP", "userAgent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36,gzip(gfe)", "clientName": "WEB", "clientVersion": "2.20200822.00.00", "osName": "X11", "browserName": "Chrome", "browserVersion": "84.0.4147.125"}}}"#;
 
-type JsonObject = serde_json::Map<String, serde_json::Value>;
-type JsonArray = Vec<serde_json::Value>;
+type Json = serde_json::Value;
 
 fn build_video_url(video_id: &str) -> String {
     "https://www.youtube.com/watch?v=".to_owned() + video_id
@@ -28,7 +27,7 @@ fn build_api_url(continuation: &str, api_key: &str) -> String {
     )
 }
 
-fn fetch_raw_html(client: &Client, video_id: &str) -> Result<String, reqwest::Error> {
+fn fetch_raw_html(client: &Client, video_id: &str) -> reqwest::Result<String> {
     let res = client
         .get(&build_video_url(video_id))
         .header(header::USER_AGENT, USER_AGENT)
@@ -61,32 +60,27 @@ fn extract_duration_from_html(raw_html: &str) -> Option<i32> {
     }
 }
 
-fn extract_continuation_from_json(data: &JsonObject) -> Option<String> {
+fn extract_continuation_from_json(data: &Json) -> Option<&str> {
     Some(
         data.get("continuationContents")?
             .get("liveChatContinuation")?
             .get("continuations")?
-            .as_array()?
             .get(0)?
-            .as_object()?
             .get("liveChatReplayContinuationData")?
             .get("continuation")?
-            .as_str()?
-            .replace("%3D", ""),
+            .as_str()?,
     )
 }
 
-fn extract_actions_from_json(data: &JsonObject) -> Option<JsonArray> {
+fn extract_actions_from_json(data: &Json) -> Option<&Json> {
     Some(
         data.get("continuationContents")?
             .get("liveChatContinuation")?
-            .get("actions")?
-            .as_array()?
-            .to_owned(),
+            .get("actions")?,
     )
 }
 
-fn extract_timestamp_from_json(data: &serde_json::Value) -> Option<&str> {
+fn extract_timestamp_from_json(data: &Json) -> Option<&str> {
     Some(
         data.get("replayChatItemAction")?
             .get("videoOffsetTimeMsec")?
@@ -98,47 +92,49 @@ fn fetch_live_chats_once(
     client: &Client,
     continuation: &str,
     api_key: &str,
-) -> Result<Option<(String, JsonArray)>, reqwest::Error> {
+) -> reqwest::Result<Option<(String, Json)>> {
     let res = client
         .post(&build_api_url(continuation, api_key))
         .header(header::CONTENT_TYPE, "application/json")
         .body(POST_BODY)
         .send()?;
-    let data: JsonObject = serde_json::from_reader(res).unwrap();
+    let data: Json = res.json()?;
     let continuation = extract_continuation_from_json(&data);
     let actions = extract_actions_from_json(&data);
     Ok(if let (Some(c), Some(a)) = (continuation, actions) {
-        Some((c, a))
+        Some((c.to_owned().replace("%3D", ""), a.to_owned()))
     } else {
         None
     })
 }
 
-fn fetch_all_live_chats(video_id: &str) -> JsonArray {
+fn fetch_all_live_chats(video_id: &str) -> Vec<Json> {
     let client = Client::builder().cookie_store(true).build().unwrap();
     let raw_html = fetch_raw_html(&client, video_id).unwrap();
     let api_key = extract_api_key_from_html(raw_html.as_str()).unwrap();
     let mut continuation = extract_continuation_from_html(raw_html.as_str()).unwrap();
     let duration = extract_duration_from_html(raw_html.as_str()).unwrap() as f64;
 
-    let mut live_chats = Vec::with_capacity((duration / 200.0) as usize);
+    let mut live_chats = Vec::<Json>::with_capacity((duration / 200.0) as usize);
     loop {
-        let api_response = fetch_live_chats_once(&client, &continuation, &api_key).unwrap();
-        if let Some((c, mut a)) = api_response {
-            continuation = c;
-            if let Some(timestamp) = extract_timestamp_from_json(a.last().unwrap()) {
-                print!(
-                    "\rProgress: {:.2}%; Total live chats: {}",
-                    timestamp.parse::<f64>().unwrap() / duration * 100.0,
-                    live_chats.len() + a.len()
-                );
-                std::io::stdout().flush().unwrap();
+        match fetch_live_chats_once(&client, &continuation, &api_key).unwrap() {
+            Some((c, mut a)) => {
+                continuation = c;
+                let actions = a.as_array_mut().unwrap();
+                if let Some(timestamp) = extract_timestamp_from_json(actions.last().unwrap()) {
+                    print!(
+                        "\rProgress: {:.2}%; Total live chats: {}",
+                        timestamp.parse::<f64>().unwrap() / duration * 100.0,
+                        live_chats.len() + actions.len()
+                    );
+                    io::stdout().flush().unwrap();
+                }
+                live_chats.append(actions);
             }
-
-            live_chats.append(&mut a);
-        } else {
-            println!();
-            break;
+            None => {
+                println!();
+                break;
+            }
         }
     }
 
@@ -148,9 +144,9 @@ fn fetch_all_live_chats(video_id: &str) -> JsonArray {
 
 fn main() {
     print!("Live streaming id: ");
-    std::io::stdout().flush().unwrap();
+    io::stdout().flush().unwrap();
     let mut video_id = String::new();
-    std::io::stdin().read_line(&mut video_id).unwrap();
+    io::stdin().read_line(&mut video_id).unwrap();
     video_id = video_id.trim_end().to_owned();
     println!("Set target: {}", video_id);
 
@@ -161,7 +157,7 @@ fn main() {
     );
 
     let dist = "./".to_owned() + video_id.as_str() + "-chats.json";
-    let file = OpenOptions::new()
+    let file = fs::OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
